68bd0d7a44b67b6e507b45ecdf7d897a2e5cd4e2
0000 <a0000@Anvars-MacBook-Air.local>
2025-10-26 19:56:01 +0900

    feat: Setup app and run successfully

---
+++ apps/transfermarkt-api/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './components/app.module';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT_API ?? 3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
