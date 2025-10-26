c3f2d7be1015f474ca6286dea843861ac8a0ba30
0000 <a0000@Anvars-MacBook-Air.local>
2025-10-26 19:37:05 +0900

    feat: install and setup modules, packages

---
+++ dist/generated/prisma/internal/class.js
@@ -0,0 +1,63 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getPrismaClientClass = getPrismaClientClass;
+const runtime = require("@prisma/client/runtime/library");
+const config = {
+    "generator": {
+        "name": "client",
+        "provider": {
+            "fromEnvVar": null,
+            "value": "prisma-client"
+        },
+        "output": {
+            "value": "/Users/a0000/Desktop/transfermarkt-backend/generated/prisma",
+            "fromEnvVar": null
+        },
+        "config": {
+            "engineType": "library"
+        },
+        "binaryTargets": [
+            {
+                "fromEnvVar": null,
+                "value": "darwin-arm64",
+                "native": true
+            }
+        ],
+        "previewFeatures": [],
+        "sourceFilePath": "/Users/a0000/Desktop/transfermarkt-backend/prisma/schema.prisma",
+        "isCustomOutput": true
+    },
+    "relativePath": "../../prisma",
+    "clientVersion": "6.18.0",
+    "engineVersion": "34b5a692b7bd79939a9a2c3ef97d816e749cda2f",
+    "datasourceNames": [
+        "db"
+    ],
+    "activeProvider": "postgresql",
+    "postinstall": false,
+    "inlineDatasources": {
+        "db": {
+            "url": {
+                "fromEnvVar": "DATABASE_URL",
+                "value": null
+            }
+        }
+    },
+    "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?\n// Try Prisma Accelerate: https://pris.ly/cli/accelerate-init\n\ngenerator client {\n  provider = \"prisma-client\"\n  output   = \"../generated/prisma\"\n}\n\ndatasource db {\n  provider = \"postgresql\"\n  url      = env(\"DATABASE_URL\")\n}\n",
+    "inlineSchemaHash": "39ac28d186f1400e7d3fe83bcf93c2ef2c93d00f3eb4ea4ccba52368d26b476f",
+    "copyEngine": true,
+    "runtimeDataModel": {
+        "models": {},
+        "enums": {},
+        "types": {}
+    },
+    "dirname": ""
+};
+config.runtimeDataModel = JSON.parse("{\"models\":{},\"enums\":{},\"types\":{}}");
+config.engineWasm = undefined;
+config.compilerWasm = undefined;
+function getPrismaClientClass(dirname) {
+    config.dirname = dirname;
+    return runtime.getPrismaClient(config);
+}
+//# sourceMappingURL=class.js.map
---
+++ dist/generated/prisma/internal/prismaNamespaceBrowser.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TransactionIsolationLevel = exports.ModelName = exports.AnyNull = exports.JsonNull = exports.DbNull = exports.NullTypes = exports.Decimal = void 0;
+const runtime = require("@prisma/client/runtime/index-browser");
+exports.Decimal = runtime.Decimal;
+exports.NullTypes = {
+    DbNull: runtime.objectEnumValues.classes.DbNull,
+    JsonNull: runtime.objectEnumValues.classes.JsonNull,
+    AnyNull: runtime.objectEnumValues.classes.AnyNull,
+};
+exports.DbNull = runtime.objectEnumValues.instances.DbNull;
+exports.JsonNull = runtime.objectEnumValues.instances.JsonNull;
+exports.AnyNull = runtime.objectEnumValues.instances.AnyNull;
+exports.ModelName = {};
+exports.TransactionIsolationLevel = runtime.makeStrictEnum({
+    ReadUncommitted: 'ReadUncommitted',
+    ReadCommitted: 'ReadCommitted',
+    RepeatableRead: 'RepeatableRead',
+    Serializable: 'Serializable'
+});
+//# sourceMappingURL=prismaNamespaceBrowser.js.map
